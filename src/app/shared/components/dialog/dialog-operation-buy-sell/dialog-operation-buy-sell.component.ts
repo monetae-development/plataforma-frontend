e9a7f60fddff6e7f7afc6d0c9245990f3fb8c3d3
jminayamonetae <jose.minaya@monetae.io>
2024-01-13 13:25:31 -0500

    rediseign purchase

--- src/app/shared/components/dialog/dialog-operation-buy-sell/dialog-operation-buy-sell.component.ts
+++ src/app/shared/components/dialog/dialog-operation-buy-sell/dialog-operation-buy-sell.component.ts
@@ -1,8 +1,13 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, OnInit } from '@angular/core';
-import { MenuItem } from 'primeng/api';
+import { Component, EventEmitter, Injector, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AppComponentBase } from '@shared/common/app-component-base';
+import { ServiceCommonProxy } from '@shared/service-proxies/service-common-proxies';
+import { MenuItem, SelectItem } from 'primeng/api';
 import { ButtonModule } from 'primeng/button';
+import { DropdownModule } from 'primeng/dropdown';
 import { DynamicDialogConfig, DynamicDialogRef } from 'primeng/dynamicdialog';
+import { InputNumberModule } from 'primeng/inputnumber';
 import { TabMenuModule } from 'primeng/tabmenu';
 
 @Component({
@@ -12,23 +17,39 @@ import { TabMenuModule } from 'primeng/tabmenu';
   styleUrls: ['./dialog-operation-buy-sell.component.css'],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     TabMenuModule,
-    ButtonModule 
+    ButtonModule,
+    DropdownModule,
+    InputNumberModule 
   ]
 })
-export class DialogOperationBuySellComponent implements OnInit {
+export class DialogOperationBuySellComponent  extends AppComponentBase implements OnInit {
 
   outAccept = new EventEmitter();
+  purchaseForm: FormGroup;
+  saleForm: FormGroup;
 
   menuItems: MenuItem[] | undefined;
   activeItem: MenuItem | undefined;
   activeIndex: Number = 0;
+  cryptoAssets: SelectItem[] = undefined;
+  amount: number = 0;
+  amountCommision: number = 0;
+  comission: number = 0;
+  coinSubtitle: string = '';
 
   constructor(
+    injector: Injector,
+    private fb: FormBuilder,
     public ref: DynamicDialogRef,
-    public config: DynamicDialogConfig
+    public config: DynamicDialogConfig,
+    private _serviceCommonProxy: ServiceCommonProxy,
   ) { 
+    super(injector);
     this.activeIndex = config.data?.activeIndex;
+    this.purchaseForm = this._buildPurchaseForm();
+    this.saleForm = this._buildSaleForm();
   }
 
   ngOnInit() {
@@ -37,18 +58,80 @@ export class DialogOperationBuySellComponent implements OnInit {
       { label: 'Vender' }
     ];
     this.activeItem = this.menuItems[Number(this.activeIndex)];
+    this.loadCryptoAssets();
+  }
+
+  private _buildPurchaseForm(): FormGroup {
+    return this.fb.group({
+      cryptoAssetId: [{ value: null, disabled: true}, [Validators.required]],
+      amount: [null, [Validators.required]],
+    });
+  }
+
+  private _buildSaleForm(): FormGroup {
+    return this.fb.group({
+      cryptoAssetId: [{ value: null, disabled: true}, [Validators.required]],
+      amount: [null, [Validators.required]],
+    });
+  }
+
+  get cryptoAssetIdPurchaseControl() { return this.purchaseForm.controls['cryptoAssetId'] as FormControl; }
+  get amountPurchaseControl() { return this.purchaseForm.controls['amount'] as FormControl; }
+
+  get cryptoAssetIdSaleControl() { return this.saleForm.controls['cryptoAssetId'] as FormControl; }
+  get amountSaleControl() { return this.saleForm.controls['amount'] as FormControl; }
+
+  loadCryptoAssets(){
+    this._serviceCommonProxy.getSelectOptions('OTCTrading/GetAllCryptoCoins', null)
+      .subscribe((result) => {
+        this.cryptoAssets = result.items;
+        this.cryptoAssetIdPurchaseControl.enable();
+        this.cryptoAssetIdSaleControl.enable();
+      });
   }
 
   onCancel(){
     this.ref.close();
   }
 
-  onAccept(): void{
-    this.outAccept.emit(true);
+  onChangeCurrencyPurchase(event: any) {
+    if (event.value == null) {
+      this.coinSubtitle = null;
+      return;
+    }
+    this.coinSubtitle = event.value.subtitle;
+  }
+
+  amountPurchaseOnChange(event: any) {
+    if (event.value == null) {
+      this.amountCommision = null;
+      return;
+    }
+    this.amountPurchaseControl.setValue(event.value);
+    this.calculateCost();
+  }
+
+  calculateCost() {
+    if (this.amount) {
+      this.amountCommision = (this.amountPurchaseControl.value * this.comission) / 100;
+    } else {
+      this.amountCommision = undefined;
+    }
   }
 
   onActiveItemChange(event: MenuItem) {
     this.activeItem = event;
+  }
+
+  onContinuePurchase(): void {
+
+  }
+  onContinueSale(): void {
+
+  }
+
+  setMaxAmount(): void {
+    this.amountPurchaseControl.setValue(this.amount);
   }
 
 }
