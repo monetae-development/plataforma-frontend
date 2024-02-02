531ff10c48ba6b8d78e6e81995f5e869eb9d93ca
Fernando <fernando@monetae.io>
2024-02-02 14:56:03 -0600

    feature: Descarga de archivo de comprobande de depósito

--- src/app/main/members/mntMemberFiatRequests/view-memberFiatRequest-modal.component.ts
+++ src/app/main/members/mntMemberFiatRequests/view-memberFiatRequest-modal.component.ts
@@ -1,10 +1,17 @@
 import { AppConsts } from '@shared/AppConsts';
 import { Component, ViewChild, Injector, Output, EventEmitter, OnInit } from '@angular/core';
 import { ModalDirective } from 'ngx-bootstrap/modal';
-import { RequestType } from '@shared/service-proxies/enum/OTC/RequestType.enum';
-import { RequestStatus } from '@shared/service-proxies/enum/OTC/RequestStatus.enum';
 import { MntMemberFiatFullDto } from '@shared/service-proxies/dto/members/mntMemberFiat/MntMemberFiatFullDto';
+import { MntMemberBankAccountDto } from '@shared/service-proxies/dto/members/mntMemberBankAccount/MntMemberBankAccountDto';
+import { PlatformBankAccountForViewMemberDto } from '@shared/service-proxies/dto/Platform/PlatformBankAccount/PlatformBankAccountForViewMemberDto';
+import { UserInfoDto } from '@shared/service-proxies/dto/Authorization/Users/UserInfoDto';
+import { MntMemberFileDto } from '@shared/service-proxies/dto/members/mntMemberFile/MntMemberFileDto';
+import { FiatType } from '@shared/service-proxies/enum/Members/FiatType.enum';
+import { FiatStatus } from '@shared/service-proxies/enum/Members/FiatStatus.enum';
+import { FileType } from '@shared/service-proxies/enum/Members/FileType.enum';
+import { DateTime } from 'luxon';
 import { AppComponentBase } from '@shared/common/app-component-base';
+import { ServiceMembersProxy } from '@shared/service-proxies/service-members-proxies';
 
 @Component({
   selector: 'member-fiat-request-modal',
@@ -14,19 +21,37 @@ export class ViewMemberFiatRequestModalComponent extends AppComponentBase implem
   @ViewChild('viewRequestModal', { static: true }) modal: ModalDirective;
 
   active = false;
+  record: MntMemberFiatFullDto;
+  typeRequest = FiatType;
+  fileType = FileType;
 
   constructor(
-    injector: Injector
+    injector: Injector,
+    private _mntMembersProxy: ServiceMembersProxy,
   ) {
     super(injector);
 
   }
   ngOnInit() {
+    this.record = new MntMemberFiatFullDto();
+    this.record.mntMemberBankAccountFk = new MntMemberBankAccountDto();
+    this.record.platformBankAccountFk = new PlatformBankAccountForViewMemberDto();
+    this.record.userFk = new UserInfoDto();
+    this.record.mntMemberFileFk = new MntMemberFileDto();
   }
+
   show(record: MntMemberFiatFullDto): void {
-    console.log(record);
+    this.record = record;
     this.active = true;
     this.modal.show();
+  }
 
+  close(): void {
+    this.active = false;
+    this.modal.hide();
+  }
+
+  downloadFile(fileType: number): void {
+    this._mntMembersProxy.downloadMemberFile(this.record.userFk.id, fileType);
   }
 }
