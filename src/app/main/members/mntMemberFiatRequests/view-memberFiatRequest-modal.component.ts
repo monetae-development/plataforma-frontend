375ebc3a80feb3a089346d3d575508fa1f0853d7
Fernando <fernando@monetae.io>
2024-02-02 16:19:51 -0600

    fixbug:Corrección de endpoint para descarga de archivo de depósito

--- src/app/main/members/mntMemberFiatRequests/view-memberFiatRequest-modal.component.ts
+++ src/app/main/members/mntMemberFiatRequests/view-memberFiatRequest-modal.component.ts
@@ -52,6 +52,6 @@ export class ViewMemberFiatRequestModalComponent extends AppComponentBase implem
   }
 
   downloadFile(fileId: number): void {
-    this._mntMembersProxy.downloadMemberFile(this.record.userFk.id, fileId);
+    this._mntMembersProxy.downloadDepositFile(this.record.userFk.id, fileId);
   }
 }
--- src/shared/service-proxies/service-members-proxies.ts
+++ src/shared/service-proxies/service-members-proxies.ts
@@ -1145,7 +1145,7 @@ export class ServiceMembersProxy {
     downloadDepositFile(userId: number | undefined, fileId: number | undefined): void {
         let url_ = this.baseUrl + '/api/services/app/MntMemberFiles/DownloadDepositFile?';
         url_ += 'userId=' + encodeURIComponent('' + ((userId === null || userId === undefined) ? '' : userId)) + '&';
-        url_ += 'type=' + encodeURIComponent('' + ((fileId === null || fileId === undefined) ? '' : fileId)) + '&';
+        url_ += 'fileId=' + encodeURIComponent('' + ((fileId === null || fileId === undefined) ? '' : fileId)) + '&';
         url_ = url_.replace(/[?&]$/, '');
         location.href = url_;
     }
